8581633f97af173fcb3852559f30ec16101a68c9
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2020-03-12 23:26:39 -0500

    Navbar added and waves containter started

---
+++ src/components/FluidContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+const FluidContainer = () => {
+    return (
+        <div class="wrapper">
+        <div class="wave"></div>
+      </div>
+    )
+}
+
+export default FluidContainer
